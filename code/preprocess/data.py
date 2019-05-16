78a22c4d8a4ff6dedd1bf618a438f2dbe4479ded
sethjuarez <me@sethjuarez.com>
2019-05-16 22:02:34 +0200

    added preprocessing code (verified local run)

--- code/preprocess/data.py
+++ code/preprocess/data.py
@@ -1,9 +1,45 @@
 import os
 import time
+import shutil
+import urllib
+import zipfile
 import argparse
 import numpy as np
 import tensorflow as tf
 from pathlib import Path
+
+def check_dir(path, check=False):
+    if check:
+        assert os.path.exists(path), '{} does not exist!'.format(path)
+    else:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return Path(path).resolve()
+
+def download(source, target, force_clear=False):
+    if force_clear and os.path.exists(target):
+        print('Removing {}...'.format(target))
+        shutil.rmtree(target)
+
+    check_dir(target)
+    
+    targt_file = str(Path(target).joinpath('data.zip'))
+    if os.path.exists(targt_file) and not force_clear:
+        print('data already exists, skipping download')
+        return
+
+    if source.startswith('http'):
+        print("Downloading from {} to {}".format(source, target))
+        urllib.urlretrieve(source, targt_file)
+        print("Done!")
+    else:
+        print("Copying from {} to {}".format(source, target))
+        shutil.copyfile(source, targt_file)
+
+    print('Unzipping {}'.format(targt_file))
+    zipr = zipfile.ZipFile(targt_file)
+    zipr.extractall(target)
+    zipr.close()
 
 def process_image(path, image_size):
     img_raw = tf.io.read_file(path)
@@ -27,40 +63,52 @@ def walk_images(base_path, image_size=160):
         print('Processing {}'.format(path))
         # only care about files in directory
         for item in os.listdir(path):
-            if not item.endswith('.jpg'):
-                print('\nskipping {}'.format(item))
+            if not item.lower().endswith('.jpg'):
+                print('skipping {}'.format(item))
                 continue
 
             image = os.path.join(path, item)
-            print(image)
             try:
                 img = process_image(image, image_size)
                 assert img.shape[2] == 3, "Invalid channel count"
                 # write out good images
                 images.append(image)
             except Exception as e:
-                print('\n{}\n{}\n'.format(e, image))
+                print('{}\n{}\n'.format(e, image))
 
     return images
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description='data cleaning for binary image task')
-    parser.add_argument('-b', '--base_path', help='directory to base data', default='..')
-    parser.add_argument('-d', '--data', help='directory to training and test data', default='data')
-    parser.add_argument('-t', '--target', help='target file to hold good data', default='dataset.txt')
+    parser.add_argument('-b', '--base_path', help='directory to base data', default='../../data')
+    parser.add_argument('-d', '--data', help='directory to training data', default='train')
+    parser.add_argument('-t', '--target', help='target file to hold good data', default='train.txt')
     parser.add_argument('-i', '--img_size', help='target image size to verify', default=160, type=int)
+    parser.add_argument('-z', '--zipfile', help='source data zip file', default='../../tacodata.zip')
+    parser.add_argument('-f', '--force', help='force clear all data', default=False, action='store_true')
     args = parser.parse_args()
+    print(args)
 
     print('Using TensorFlow v.{}'.format(tf.__version__))
-    
-    base_path = Path(args.base_path).joinpath(args.data).resolve()
-    target_path = Path(base_path).resolve().joinpath(args.target)
 
-    images = walk_images(str(base_path), args.img_size)
+    base_path = Path(args.base_path).resolve()
+    print('Base Path:  {}'.format(base_path))
+    data_path = base_path.joinpath(args.data).resolve()
+    print('Train Path: {}'.format(data_path))
+    target_path = Path(base_path).resolve().joinpath(args.target)
+    print('Train File: {}'.format(target_path))
+    zip_path = Path(args.zipfile)
+
+    print('Acquiring data...')
+    download(str(zip_path), str(base_path), args.force)
+    print('Testing images...')
+    images = walk_images(str(data_path), args.img_size)
 
     # save file
     print('writing dataset to {}'.format(target_path))
     with open(str(target_path), 'w+') as f:
         f.write('\n'.join(images))
 
-    # python data.py -d data/PetImages -t dataset.txt
+    # python data.py -d data/PetImages -t train.txt
+    # python data.py -d data/train -t dataset.txt
