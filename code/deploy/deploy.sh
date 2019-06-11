4a550e40d3d4c1a51c2bce168e3865af702b8c77
Seth Juarez <seth.juarez@microsoft.com>
2019-06-11 16:56:20 -0700

    Revert "pipeline errors corrected"

--- code/deploy/deploy.sh
+++ code/deploy/deploy.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 az ml model deploy -n tacosandburritos -m tacosandburritos:6 --ic inferenceconfig.json --dc deploymentconfig.json --resource-group kubeflow-mlops-rg --workspace-name kfaml --overwrite -v
+=======
+az ml model deploy --name tacosandburritos --ic ./inferenceconfig.json --dc ./deploymentconfig.json --model tacosandburritos:6 --overwrite -v
+>>>>>>> e2658a9e036a69de526ebff22d46de7157b73fc1
